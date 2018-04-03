9ceb657a6ff303c1901e0046de354241892d138f
“avalanchesiqi” <wusiqi.china@gmail.com>
2018-04-03 23:54:55 +1000

    more run_all_predictors bug fixing

--- engagement_prediction/run_all_predictors.sh
+++ engagement_prediction/run_all_predictors.sh
@@ -15,51 +15,51 @@ fi
 log_file="$target"_predictions.log
 
 if [ "$target" == "wp" ]; then
-  python extract_channel_reputation.py -i ./ -o ./output/train_channel_watch_percentage.txt -f "$target" >> log_file
+  python extract_channel_reputation.py -i ./ -o ./output/train_channel_watch_percentage.txt -f "$target" >> "$log_file"
 else
-  python extract_channel_reputation.py -i ./ -o ./output/train_channel_relative_engagement.txt -f "$target" >> log_file
+  python extract_channel_reputation.py -i ./ -o ./output/train_channel_relative_engagement.txt -f "$target" >> "$log_file"
 fi
 
 sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
 
-python duration_predictor.py -i ./ -o ./output -f "$target" >> log_file
-
-sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
-
-python context_predictor.py -i ./ -o ./output -f "$target" >> log_file
+python duration_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
 
 sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
 
-python sparse_topic_predictor.py -i ./ -o ./output -f "$target" >> log_file
-
-sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
-
-python sparse_context_topic_predictor.py -i ./ -o ./output -f "$target" >> log_file
+python context_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
 
 sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
 
-python reputation_predictor.py -i ./ -o ./output -f "$target" >> log_file
-
-sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
-
-python python sparse_all_predictor.py -i ./ -o ./output -f "$target" >> log_file
+python sparse_topic_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
 
 sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
 
-python python channel_specific_predictor.py -i ./ -o ./output -f "$target" >> log_file
+python sparse_context_topic_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
 
 sleep 60
-echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> log_file
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
+
+python reputation_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
+
+sleep 60
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
+
+python python sparse_all_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
+
+sleep 60
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
+
+python python channel_specific_predictor.py -i ./ -o ./output -f "$target" >> "$log_file"
+
+sleep 60
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' >> "$log_file"
 
 if [ "$target" == "wp" ]; then
-  python python construct_pandas_frame.py -i ./output -o ./output/predicted_wp_df.csv -f "$target" >> log_file
+  python python construct_pandas_frame.py -i ./output -o ./output/predicted_wp_df.csv -f "$target" >> "$log_file"
 else
-  python python construct_pandas_frame.py -i ./output -o ./output/predicted_re_df.csv -f "$target" >> log_file
+  python python construct_pandas_frame.py -i ./output -o ./output/predicted_re_df.csv -f "$target" >> "$log_file"
 fi
